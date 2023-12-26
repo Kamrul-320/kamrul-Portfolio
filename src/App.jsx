bb276262ae57c7de34401de97099b0154fec55ff
Kamrul-320 <144392155+Kamrul-320@users.noreply.github.com>
2023-12-27 00:32:06 +0600

    navbar done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './app.css' 
+import Navbar from './components/Navbar/Navbar'
+
+const App = () => {
+  return (
+    <div>
+      <section>
+      <Navbar/>
+      </section>
+    </div>
+  )
+}
+
+export default App
